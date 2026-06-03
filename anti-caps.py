baaf120b304c1e8ef04291445e01ac98c6858400
Underscore-163 <reuben@dearden.org.uk>
2026-06-03 14:15:47 +0100

    Fixed the app crashing because the icon is not present. Oops!

--- anti-caps.py
+++ anti-caps.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import pystray
 import pyautogui as gui
 from PIL import Image
@@ -17,6 +19,22 @@ def quit(*args):
     icon.stop()
 
 
+# Source - https://stackoverflow.com/a/13790741
+# Posted by max, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-06-03, License - CC BY-SA 3.0
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
 menu = pystray.Menu(
     pystray.MenuItem(
         text="Pause",
@@ -27,11 +45,10 @@ menu = pystray.Menu(
         text="Exit",
         action=quit,
     )
-
 )
 
 icon = pystray.Icon(name="Anti-Caps",
-                    icon=Image.open("anti-caps.png"),
+                    icon=Image.open(resource_path("anti-caps.png")),
                     menu=menu, )
 
 icon.run_detached()
